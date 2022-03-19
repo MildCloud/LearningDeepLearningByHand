919cf5edea513704907551de77f0537ff1a10fcd
tian-shu <tian-shu@sjtu.edu.cn>
2022-03-19 17:12:51 +0800

    Translation Data Set

--- TranslationDataSet/main.py
+++ TranslationDataSet/main.py
@@ -1,8 +1,8 @@
-import enum
+import collections
 import os
-from tkinter.messagebox import NO
 import torch
 from d2l import torch as d2l
+from torch.utils import data
 
 
 d2l.DATA_HUB['fra-eng'] = (d2l.DATA_URL + 'fra-eng.zip',
@@ -16,7 +16,7 @@ def read_data_nmt():
         return f.read()
 
 
-raw_text = read_data_nmt()
+# raw_text = read_data_nmt()
 # The type of raw is string
 
 
@@ -35,7 +35,7 @@ def preprocess_nmt(text):
     return ''.join(out)
 
 
-text = preprocess_nmt(raw_text)
+# text = preprocess_nmt(raw_text)
 
 
 def tokenize_nmt(text, num_examples=None):
@@ -51,5 +51,99 @@ def tokenize_nmt(text, num_examples=None):
     return source, target
 
 
-source, target = tokenize_nmt(text)
-print(source[:6], target[:6])
+# source, target = tokenize_nmt(text)
+# source and target are two dimensional lists and the second dimension is 2
+
+
+class Vocab:
+    """Vocabulary for text."""
+    def __init__(self, tokens=None, min_freq=0, reserved_tokens=None):
+        if tokens is None:
+            tokens = []
+        if reserved_tokens is None:
+            reserved_tokens = []
+        counter = count_corpus(tokens)
+        self.token_freqs = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+        self.unk, uniq_tokens = 0, ['<unk>'] + reserved_tokens
+        uniq_tokens += [
+            token for token, freq in self.token_freqs
+            if freq >= min_freq and token not in uniq_tokens
+        ]
+        self.idx_to_token, self.token_to_idx = [], dict()
+        for token in uniq_tokens:
+            self.idx_to_token.append(token)
+            self.token_to_idx[token] = len(self.idx_to_token) - 1
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[index] for index in indices]
+
+
+def count_corpus(tokens):
+    """Count token frequencies."""
+    if len(tokens) == 0 or isinstance(tokens[0], list):
+        tokens = [token for line in tokens for token in line]
+    return collections.Counter(tokens)
+
+
+# src_vocab = Vocab(source, min_freq=2, reserved_tokens=['<pad>', '<bos>', '<eos>'])
+# print(src_vocab['<pad>'])
+
+
+def truncate_pad(line, num_steps, padding_token):
+    """Truncate or pad Sequence"""
+    if len(line) > num_steps:
+        return line[:num_steps]
+    return line + [padding_token] * (num_steps - len(line))
+
+
+def build_array_nmt(lines, vocab, num_steps):
+    """Transform text sequences of machine translation into minibatches"""
+    lines = [vocab[l] for l in lines]
+    print('lines.shape() = ', len(lines), len(lines[0]))
+    lines = [l + [vocab['<eos>']] for l in lines]
+    array = torch.tensor([
+        truncate_pad(l, num_steps, vocab['<pad>'])
+        for l in lines
+    ])
+    valid_len = (array != vocab['<pad>']).type(torch.int32).sum(1)
+    return array, valid_len
+
+
+def load_array(data_arrays, batch_size, is_train=True):
+    """Construct a PyTorch data iterator. Defined in :numref:`sec_linear_concise`"""
+    dataset = data.TensorDataset(*data_arrays)
+    return data.DataLoader(dataset, batch_size, shuffle=is_train)
+
+
+def load_data_nmt(batch_size, num_steps, num_examples=600):
+    """Return the iterator and the vocabularies of the translation dataset."""
+    text = preprocess_nmt(read_data_nmt())
+    source, target = tokenize_nmt(text, num_examples)
+    src_vocab = Vocab(source, min_freq=2, reserved_tokens=['<pad>', '<bos>', '<eos>'])
+    tgt_vocab = Vocab(target, min_freq=2, reserved_tokens=['<pad>', '<bos>', '<eos>'])
+    src_array, src_valid_len = build_array_nmt(source, src_vocab, num_steps)
+    print('src_array.shape = ', src_array.shape)
+    print('src_valid_len.shape = ', len(src_valid_len), src_valid_len[0])
+    tgt_array, tgt_valid_len = build_array_nmt(target, tgt_vocab, num_steps)
+    data_arrays = (src_array, src_valid_len, tgt_array, tgt_valid_len)
+    data_iter = load_array(data_arrays, batch_size)
+    return data_iter, src_vocab, tgt_vocab
+
+
+train_iter, src_vocab, tgt_vocab = load_data_nmt(batch_size=2, num_steps=8)
+for X, X_valid_len, Y, Y_valid_len in train_iter:
+    print('X:', X.type(torch.int32))
+    print('valid lengths for X:', X_valid_len)
+    print('Y:', Y.type(torch.int32))
+    print('valid lengths for Y:', Y_valid_len)
+    break
